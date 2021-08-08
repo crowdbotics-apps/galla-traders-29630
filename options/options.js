c50720d0a3a2f7ef403b79948730b9f4286d5c2b
Crowdbotics <team@crowdbotics.com>
2021-08-08 10:08:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "galla_traders_29630Identifier",
+  url: "https://galla_traders_29630Identifier.botics.co",
+  api: "https://galla_traders_29630Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
